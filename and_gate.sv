57f9cd7b5d848c0fc409bdbda45b03c9aa0a216c
raja k <rajak@training-srv5>
2025-11-26 14:09:09 +0530

    pushed line number line num 3

--- and_gate.sv
+++ and_gate.sv
@@ -1,6 +1,7 @@
 //and gate coding
 //added extra line after clone to do pull in local repo
-
+/*	added this line after git pull(sucess) to get changes from remote and pushed
+ 	into main from local repo.*/
 module and_g(y,a,b);
 	input a,b;
 	output wire y;
